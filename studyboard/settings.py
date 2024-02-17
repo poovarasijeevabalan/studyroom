13300c1c229165b8a10e444cc1e9d974a95fc783
poovarasijeevabalan <poovarasijeevabalan@gmail.com>
2024-02-17 07:37:48 +0530

    changed setting

--- studyboard/settings.py
+++ studyboard/settings.py
@@ -109,10 +109,10 @@ DATABASES = {
 
         
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'de18sntmm49g52',
-        'USER': 'jzfodhbkxyldqj',
-        'PASSWORD': 'e6020d1c4843f719877bc50cdb3769277d82b2f31b9bcec7bc96c99f66d37bf6',
-        'HOST': 'ec2-34-193-110-25.compute-1.amazonaws.com',  # or the IP address of your PostgreSQL server
+        'NAME': 'de18s***9g52',
+        'USER': 'jzfo****ldqj',
+        'PASSWORD': 'e6020d****77d82b2f31b9bcec7bc96c99f66d37bf6',
+        'HOST': 'ec2-****ute-1.amazonaws.com',  # or the IP address of your PostgreSQL server
         'PORT': '5432',  # default PostgreSQL port
 
     }
