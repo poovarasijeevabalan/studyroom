bb14b4c29e7e0f26728f9771980f4065ff44ede9
poovarasijeevabalan <poovarasijeevabalan@gmail.com>
2024-02-17 07:35:49 +0530

    changed title

--- base/models.py
+++ base/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here. ORM - 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=70)
 
     def __str__(self):
         return self.name
